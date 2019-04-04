255310491f7333c48cba09580319a650e8f2220d
qczl091 <sudr@ccidgroup.com>
2019-04-04 16:28:56 +0800

    study the basics of nuxt.js

--- pages/users/_id/index.vue
+++ pages/users/_id/index.vue
@@ -1,3 +1,13 @@
 <template>
     <h1>{{$route.params.id}}</h1>
 </template>
+<script>
+export default {
+    //nuxt js run it before render the route
+    validate(data){
+        console.log(data);
+        //测试是不是数字
+        return /^\d+$/.test(data.params.id);
+    }
+}
+</script>
