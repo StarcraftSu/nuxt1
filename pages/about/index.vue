5a8cd7c7c47e2356e71f0abbc920393070608c9d
qczl091 <sudr@ccidgroup.com>
2019-04-08 09:52:05 +0800

    set the basic structure for blog project

---
+++ pages/about/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <div class="about-page">
+            <h1>About me page</h1>
+            <p>dummy text</p>
+            <p>dummy text</p>
+            <p>dummy text</p>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+    .about-page{
+        text-align: center;
+        padding:30px;
+    }
+</style>
